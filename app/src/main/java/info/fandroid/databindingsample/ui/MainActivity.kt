2b6bd40978eb19bf421ef243ae8969ae37fbcd05
Sergey <Belyakovsergeyrus@yandex.ru>
2020-05-21 13:41:23 +0300

    Add files via upload

---
+++ app/src/main/java/info/fandroid/databindingsample/ui/MainActivity.kt
@@ -0,0 +1,79 @@
+package info.fandroid.databindingsample.ui
+
+import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.widget.ImageViewCompat
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
+import info.fandroid.databindingsample.R
+import info.fandroid.databindingsample.data.Popularity
+import info.fandroid.databindingsample.data.SimpleViewModel
+import info.fandroid.databindingsample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    // Obtain ViewModel from ViewModelProviders
+    private val viewModel by lazy { ViewModelProviders.of(this).get(SimpleViewModel::class.java) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding : ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.name = "Your name"
+        binding.lastName = "Your last name"
+
+        // TODO: Explicitly setting initial values is a bad pattern. We'll fix that.
+
+        updateLikes()
+    }
+
+    fun onLike(view: View) {
+        viewModel.onLike()
+        updateLikes()
+    }
+
+    private fun updateLikes() {
+        findViewById<TextView>(R.id.likes).text = viewModel.likes.toString()
+        findViewById<ProgressBar>(R.id.progressBar).progress =
+            (viewModel.likes * 100 / 5).coerceAtMost(100)
+        val image = findViewById<ImageView>(R.id.imageView)
+
+        val color = getAssociatedColor(viewModel.popularity, this)
+
+        ImageViewCompat.setImageTintList(image, ColorStateList.valueOf(color))
+
+        image.setImageDrawable(getDrawablePopularity(viewModel.popularity, this))
+    }
+
+    private fun getAssociatedColor(popularity: Popularity, context: Context): Int {
+        return when (popularity) {
+            Popularity.NORMAL -> context.theme.obtainStyledAttributes(
+                intArrayOf(android.R.attr.colorForeground)
+            ).getColor(0, 0x000000)
+            Popularity.POPULAR -> ContextCompat.getColor(context, R.color.popular)
+            Popularity.STAR -> ContextCompat.getColor(context, R.color.star)
+        }
+    }
+
+    private fun getDrawablePopularity(popularity: Popularity, context: Context): Drawable? {
+        return when (popularity) {
+            Popularity.NORMAL -> {
+                ContextCompat.getDrawable(context, R.drawable.ic_person_black_96dp)
+            }
+            Popularity.POPULAR -> {
+                ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
+            }
+            Popularity.STAR -> {
+                ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
+            }
+        }
+    }
+}
